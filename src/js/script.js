fb5f7bc73877376c3d0bd7af15102dba96e1896d
Vladyslav Sazonov <nerop30@gmail.com>
2016-10-03 09:42:34 +0300

    deleted untapped code

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,4 @@
 $(function () {
-    var images = [
-        './img/0.jpg',
-        './img/1.jpg',
-        './img/2.jpg',
-        './img/3.jpg',
-        './img/4.jpg',
-        './img/5.jpg',
-        './img/6.jpg',
-        './img/7.jpg',
-        './img/8.jpg',
-        './img/9.jpg'
-    ];
 
     var dataImages = [];
 
@@ -33,7 +21,6 @@ $(function () {
         }
     });
 
-
     var currentImage = 0;
     var imageContainer = document.getElementsByClassName('container');
     var imageHolder = document.getElementsByClassName('img-holder');
@@ -50,7 +37,6 @@ $(function () {
             imageContainer[0].appendChild(img);
         }
     }
-
 
     function closeModal() {
         $('.modal').addClass('modal-close-animator');
@@ -155,5 +141,4 @@ $(function () {
             $('.loader').css('visibility', 'visible');
         }).bind(this);
     }
-
 });
